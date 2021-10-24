8ebf1e5fbb1945468f3bdf5565f9dfa673fa5f1e
Hemish <hemish04082005@gmail.com>
2021-10-25 01:27:17 +0530

    Added new scripts

---
+++ chrome-flag-gen.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+flags = ["password-store=gnome"]
+
+from os import environ
+from os.path import expanduser as exp
+from os.path import join
+from os import remove
+
+# Remove if chrome-flags.conf exists
+try:
+    remove(join(exp("~"), ".config", "chrome-flags.conf"))
+except:
+    pass # Pass if doesn't exist
+
+environment = environ["XDG_SESSION_TYPE"]
+if environment == "wayland":
+	flags.append("enable-features=UseOzonePlatform")
+	flags.append("ozone-platform=wayland")
+
+with open(join(exp("~"), ".config", "chrome-flags.conf"), "a") as file:
+	for item in flags:
+		file.write("--"+item+"\n")
+
